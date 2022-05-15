079278557f4f70d1e2463eadfb613409eba3c6e9
Yaozhtj <1697661410@qq.com>
2022-05-15 23:43:09 +0800

    -ey add the description section

--- src/Description.jsx
+++ src/Description.jsx
@@ -7,8 +7,23 @@ export default function Description() {
 
   const grammarRules = [
     '@declear: a|b|c|... 所有的终结符声明',
-    'A->B|C|...: 产生式形式，A可推导B或C或更多',
-    'S: 拓展文法非终结符，文法规则至少应含有该项'
+    'A->B|C|...: 产生式形式,A可推导B或C或更多',
+    's: 拓展文法非终结符，文法规则至少应含有该项'
+  ]
+
+  const grammarDescriptions = [
+    'program: 主程序',
+    'fun_dec: 函数声明',
+    'var_dec: 变量声明',
+    'param_dec: 函数参数声明',
+    'assign_stmt: 赋值语句',
+    'return_stmt: 返回语句',
+    'if_stmt: if条件语句',
+    'while_stmt: while循环语句',
+    'call_stmt: 调用语句',
+    'add_sub_exp: 加减表达式',
+    'item: 项',
+    'factor: 因子'
   ]
 
   return (
@@ -41,6 +56,16 @@ export default function Description() {
         </p>
         <p>
           {grammarRules.map(r=>{
+            return (
+              <div className="d-section">{r}</div>
+            )
+          })}
+        </p>
+        <p>
+          The grammar description are below:
+        </p>
+        <p>
+          {grammarDescriptions.map(r=>{
             return (
               <div className="d-section">{r}</div>
             )
